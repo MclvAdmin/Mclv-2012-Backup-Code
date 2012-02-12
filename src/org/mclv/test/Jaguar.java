af194f37805364f3c46ce62433d935d0a996762f
Alex Roman <computeralex@gmail.com>
2012-02-11 23:20:12 -0500

    First commit, tested basic code

---
+++ src/org/mclv/test/Jaguar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.mclv.test;
+
+/**
+ *
+ * @author God
+ */
+public class Jaguar { //DUMMYJAG
+    private int port;
+    public Jaguar(int port){
+        this.port = port;
+    }
+    public void set(double val){
+        //System.out.println("Jaguar\nID\tOutput\n" + port + "\t" + val);
+    }
+}
